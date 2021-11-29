1bf36baa4c52d16c2c7c9e4699a49536625f81ad
Colin Schmierer <colin6442@gmail.com>
2021-11-29 14:16:42 -0800

    triangle with movement(an example to learn openGL)

--- test.cpp
+++ test.cpp
@@ -3,7 +3,86 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include <Windows.h>
+#include <math.h>
+#include "shaderClass.h"
 
+#define PI 3.14159265
+
+void Rotate(int turn, float* length, GLfloat *vertices, float* currentAngle, double* angleA, double* angleB) {
+	*currentAngle += turn;
+	*angleA += turn;
+	*angleB += turn;
+	vertices[0] = (GLfloat)(*length * cos(*angleA * PI / 180.0) + vertices[6]);
+	vertices[1] = (GLfloat)(*length * sin(*angleA * PI / 180.0) + vertices[7]);
+	vertices[3] = (GLfloat)(*length * cos(*angleB * PI / 180.0) + vertices[6]);
+	vertices[4] = (GLfloat)(*length * sin(*angleB * PI / 180.0) + vertices[7]);
+}
+
+void Movement(double xVel, double yVel, GLfloat *vertices, float currentAngle) {
+	vertices[0] += (GLfloat)(xVel * cos(currentAngle * PI / 180.0)); vertices[1] += (GLfloat)(yVel * sin(currentAngle * PI / 180.0));
+	vertices[3] += (GLfloat)(xVel * cos(currentAngle * PI / 180.0)); vertices[4] += (GLfloat)(yVel * sin(currentAngle * PI / 180.0));
+	vertices[6] += (GLfloat)(xVel * cos(currentAngle * PI / 180.0)); vertices[7] += (GLfloat)(yVel * sin(currentAngle * PI / 180.0));
+}
+
+void inputDetection(GLFWwindow* window, GLfloat* vertices, double* xVel, double* yVel, float* length, float* currentAngle, double* angleA, double* angleB){
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+			if (*xVel < 1.05) {
+				*xVel += 0.001;
+			}
+			if (*yVel < 1.05) {
+				*yVel += 0.001;
+			}
+		}
+		else {
+			if (*xVel > 0) {
+				*xVel -= 0.0005;
+			}
+			else if (*xVel < 0) {
+				*xVel = 0;
+			}
+			if (*yVel > 0) {
+				*yVel -= 0.0005;
+			}
+			else if (*yVel < 0) {
+				*yVel = 0;
+			}
+
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+			if (*xVel > 0 || *yVel > 0) {
+				Rotate(5, length, vertices, currentAngle, angleA, angleB);
+			}
+
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+			if (*xVel > 0 || *yVel > 0) {
+				Rotate(-5, length, vertices, currentAngle, angleA, angleB);
+			}
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+			*xVel = 0;
+			*yVel = 0;
+		}
+
+		Movement(*xVel, *yVel, vertices, *currentAngle);
+		//x6,y7
+		if (vertices[6] > 1) {
+			vertices[6] = -1;//-0.001;
+		}
+		else if (vertices[6] < -1) {
+			vertices[6] = 1;//+0.001;
+		}
+		if (vertices[7] > 1) {
+			vertices[7] = -1;// -0.001;
+		}
+		else if (vertices[7] < -1) {
+			vertices[7] = 1;// +0.001;
+		}
+		Rotate(0, length, vertices, currentAngle, angleA, angleB);
+}
 
 int main(){
     int windowX = 900;
@@ -22,18 +101,52 @@ int main(){
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 	glViewport(0, 0, windowX, windowY);
 
+	glewExperimental = true;
+	glewInit();
+
 	GLfloat vertices[] = {
 		-0.05f, 0.1f, 0.0f, // Lower left corner	PointA
 		0.05f, 0.1f, 0.0f, // Lower right corner	PointB
 		0.0f, 0.0f, 0.0f // Upper corner			PointC
 	};
 
+	float currentAngle = 270;
+	float length = sqrt(pow((float)vertices[0], 2.0f) + pow((float)vertices[1], 2.0f));
+	double angleA = acos(vertices[0] / length) * 180.0/PI;
+	double angleB = acos(vertices[3] / length) * 180.0/PI;
+	double xVel = 0;
+	double yVel = 0;
+
+	Shader shaderProgram("vertexShaders/default.vert", "fragmentShaders/default.frag");
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);  
+
+
     float one = 0.07f, two = 0.13f, three = 0.17f;
 
     while (!glfwWindowShouldClose(window)) {
-
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(one, two, three, 1.0f);
+
+		inputDetection(window, vertices, &xVel, &yVel, &length, &currentAngle, &angleA, &angleB);
+
+		shaderProgram.Activate();
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
 		glfwPollEvents();
