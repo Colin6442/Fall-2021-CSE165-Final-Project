1362df4805d071745bb23d1e7afe1c0440f5a4de
Colin Schmierer <colin6442@gmail.com>
2021-11-29 17:33:31 -0800

    added second triangle

--- test.cpp
+++ test.cpp
@@ -18,70 +18,71 @@ void Rotate(int turn, float* length, GLfloat *vertices, float* currentAngle, dou
 	vertices[4] = (GLfloat)(*length * sin(*angleB * PI / 180.0) + vertices[7]);
 }
 
-void Movement(double xVel, double yVel, GLfloat *vertices, float currentAngle) {
+void Movement(double xVel, double yVel, GLfloat *vertices, GLfloat* other, float currentAngle) {
+	other[0] = vertices[0]; other[1] = vertices[1]; other[2] = vertices[2]; other[3] = vertices[3]; 
+	other[4] = vertices[4]; other[5] = vertices[5]; other[6] = vertices[6]; other[7] = vertices[7]; 
+	
 	vertices[0] += (GLfloat)(xVel * cos(currentAngle * PI / 180.0)); vertices[1] += (GLfloat)(yVel * sin(currentAngle * PI / 180.0));
 	vertices[3] += (GLfloat)(xVel * cos(currentAngle * PI / 180.0)); vertices[4] += (GLfloat)(yVel * sin(currentAngle * PI / 180.0));
 	vertices[6] += (GLfloat)(xVel * cos(currentAngle * PI / 180.0)); vertices[7] += (GLfloat)(yVel * sin(currentAngle * PI / 180.0));
+
 }
 
-void inputDetection(GLFWwindow* window, GLfloat* vertices, double* xVel, double* yVel, float* length, float* currentAngle, double* angleA, double* angleB){
+void inputDetection(GLFWwindow* window, GLfloat* vertices, GLfloat* other, double* xVel, double* yVel, float* length, float* currentAngle, double* angleA, double* angleB){
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-			if (*xVel < 1.05) {
-				*xVel += 0.001;
-			}
-			if (*yVel < 1.05) {
-				*yVel += 0.001;
-			}
+		if (*xVel < 1.05) {
+			*xVel += 0.001;
 		}
-		else {
-			if (*xVel > 0) {
-				*xVel -= 0.0005;
-			}
-			else if (*xVel < 0) {
-				*xVel = 0;
-			}
-			if (*yVel > 0) {
-				*yVel -= 0.0005;
-			}
-			else if (*yVel < 0) {
-				*yVel = 0;
-			}
-
+		if (*yVel < 1.05) {
+			*yVel += 0.001;
 		}
-
-		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-			if (*xVel > 0 || *yVel > 0) {
-				Rotate(5, length, vertices, currentAngle, angleA, angleB);
-			}
-
+	}else{
+		if (*xVel > 0) {
+			*xVel -= 0.0005;
 		}
-
-		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-			if (*xVel > 0 || *yVel > 0) {
-				Rotate(-5, length, vertices, currentAngle, angleA, angleB);
-			}
-		}
-
-		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		else if (*xVel < 0) {
 			*xVel = 0;
+		}
+		if (*yVel > 0) {
+			*yVel -= 0.0005;
+		}else if (*yVel < 0) {
 			*yVel = 0;
 		}
+	}
 
-		Movement(*xVel, *yVel, vertices, *currentAngle);
-		//x6,y7
-		if (vertices[6] > 1) {
-			vertices[6] = -1;//-0.001;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		if (*xVel > 0 || *yVel > 0) {
+			Rotate(5, length, vertices, currentAngle, angleA, angleB);
 		}
-		else if (vertices[6] < -1) {
-			vertices[6] = 1;//+0.001;
+
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		if (*xVel > 0 || *yVel > 0) {
+			Rotate(-5, length, vertices, currentAngle, angleA, angleB);
 		}
-		if (vertices[7] > 1) {
-			vertices[7] = -1;// -0.001;
-		}
-		else if (vertices[7] < -1) {
-			vertices[7] = 1;// +0.001;
-		}
-		Rotate(0, length, vertices, currentAngle, angleA, angleB);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		*xVel = 0;
+		*yVel = 0;
+	}
+
+	Movement(*xVel, *yVel, vertices, other, *currentAngle);
+	//x6,y7
+	if (vertices[6] > 1) {
+		vertices[6] = -1;//-0.001;
+	}
+	else if (vertices[6] < -1) {
+		vertices[6] = 1;//+0.001;
+	}
+	if (vertices[7] > 1) {
+		vertices[7] = -1;// -0.001;
+	}
+	else if (vertices[7] < -1) {
+		vertices[7] = 1;// +0.001;
+	}
+	Rotate(0, length, vertices, currentAngle, angleA, angleB);
 }
 
 int main(){
@@ -110,6 +111,12 @@ int main(){
 		0.0f, 0.0f, 0.0f // Upper corner			PointC
 	};
 
+	GLfloat other[] = {
+		-0.05f, 0.1f, 0.0f, // Lower left corner	PointA
+		0.05f, 0.1f, 0.0f, // Lower right corner	PointB
+		0.0f, 0.0f, 0.0f // Upper corner			PointC
+	};
+
 	float currentAngle = 270;
 	float length = sqrt(pow((float)vertices[0], 2.0f) + pow((float)vertices[1], 2.0f));
 	double angleA = acos(vertices[0] / length) * 180.0/PI;
@@ -127,7 +134,7 @@ int main(){
 	glGenBuffers(1, &VBO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);  
@@ -139,13 +146,12 @@ int main(){
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(one, two, three, 1.0f);
 
-		inputDetection(window, vertices, &xVel, &yVel, &length, &currentAngle, &angleA, &angleB);
+		inputDetection(window, vertices, other, &xVel, &yVel, &length, &currentAngle, &angleA, &angleB);
 
 		shaderProgram.Activate();
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(other), other, GL_DYNAMIC_DRAW);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
