b07374e3ef91a3b8e91a3a1c63ed0e0c7afeeba1
austin-myhre <austinmyhre01@gmail.com>
2021-12-06 00:09:46 -0800

    finished + ratio + ur mom + fortnite chp 3

--- main.cpp
+++ main.cpp
@@ -7,71 +7,31 @@
 #include <malloc.h>
 #include "shaderClass.h"
 #include <time.h>
+#include "soil\inc\SOIL\SOIL.h"
 
-struct Square{
-	GLfloat box[18];
-	bool set = false;
 
-	void setBox(GLfloat *in){
-		for(int i = 0; i < 18; i++){
-			box[i] = in[i];
-		}
-		set = true;
-	}
-
-	void moveUp(){
-		box[1] += 0.1; box[4] += 0.1; box[7] += 0.1;
-		box[10] += 0.1; box[13] += 0.1; box[16] += 0.1; 
-	}
-
-	void moveDown(){
-		box[1] -= 0.1; box[4] -= 0.1; box[7] -= 0.1;
-		box[10] -= 0.1; box[13] -= 0.1; box[16] -= 0.1; 
-	}
-
-	void moveLeft(){
-		box[0] -= 0.1; box[3] -= 0.1; box[6] -= 0.1;
-		box[9] -= 0.1; box[12] -= 0.1; box[15] -= 0.1; 
-	}
-
-	void moveRight(){
-		box[0] += 0.1; box[3] += 0.1; box[6] += 0.1;
-		box[9] += 0.1; box[12] += 0.1; box[15] += 0.1;
-	}
-
-	void setBoxRand(){
-		float x = ((rand() % (20 * 2) - 20) / 10.0f);
-		float y = ((rand() % (20 * 2) - 20) / 10.0f);
-
-		box[0] = x; box[1] = y; box[2] =  0;
-		box[3] = x; box[4] = y - 0.1; box[5] = 0;
-		box[6] = x - 0.1; box[7] = y; box[8] = 0;
-
-		box[9] = x; box[10] = y - 0.1; box[11] = 0;
-		box[12] = x - 0.1; box[13] = y - 0.1; box[14] = 0;
-		box[15] = x - 0.1; box[16] = y; box[17] = 0;
-		set = true;
-	}
-
+enum direction{
+	LEFT = -1,
+	RIGHT = 1,
+	UP = -20,
+	DOWN = 20,
 };
 
-float detCollide(float ax, float ay, float bx, float by){
-	float dx = ax - bx;
-	float dy = ay - by;
-	float distance = sqrt((dx*dx)+(dy*dy));
-	return distance; // for collision in structs.
-}
+int currentDirection = RIGHT;
 
 struct gridSpace{
+	int index;
+	int snakeRemaining = -1;
 	bool isSnake = false;
 	bool isApple = false;
+	bool isSnakeHead = false;
 	float x;
 	float y;
 
 	GLfloat gameCoords[18];
-
-	void setGameCoords(int index){
-		float xIn = index % 20, yIn = (index - xIn)/20;
+	void setGameCoords(int in){
+		index = in;
+		float xIn = in % 20, yIn = (in - xIn)/20;
 		xIn = xIn/10 - 0.899; yIn = yIn/10 - 0.999; yIn = -yIn;
 		gameCoords[0] = xIn; gameCoords[1] = yIn; gameCoords[2] =  0;
 		gameCoords[3] = xIn; gameCoords[4] = yIn - 0.1; gameCoords[5] = 0;
@@ -81,12 +41,146 @@ struct gridSpace{
 		gameCoords[12] = xIn - 0.1; gameCoords[13] = yIn - 0.1; gameCoords[14] = 0;
 		gameCoords[15] = xIn - 0.1; gameCoords[16] = yIn; gameCoords[17] = 0;
 	}
-	//yin * 20 + xin
+
 	void setSnake(){
 		isSnake = true;
 	}
+	void removeSnake(){
+		isSnake = false;
+	}
+	void decrementSnake(){
+		snakeRemaining--;
+		if(snakeRemaining == 0){
+			isSnake = false;
+		}
+	}
+	void setSnakeHead(int length){
+		isSnakeHead = true;
+		snakeRemaining = length;
+	}
+	void removeSnakeHead(){
+		isSnakeHead = false;
+	}
+	void setApple(){
+		isApple = true;
+	}
+	void removeApple(){
+		isApple = false;
+	}
+
 
 };
+
+void spawnApple(gridSpace* grid){
+	float x = rand() % 20;
+	float y = rand() % 20;
+	int index = y*20 + x;
+	grid[index].setApple();
+}
+
+bool setMovement(gridSpace* grid, int* snakeHead, bool* eatApple, int* length){
+	switch(currentDirection){
+		case UP:
+			grid[*snakeHead].isSnakeHead = false;
+			if(grid[*snakeHead + UP].isApple){
+				*length += 1;
+				*eatApple = true;
+				grid[*snakeHead + UP].removeApple();
+			}
+			if(grid[*snakeHead + UP].isSnake){
+				return true;
+			}
+			grid[*snakeHead + UP].setSnake();
+			grid[*snakeHead + UP].setSnakeHead(*length);
+			*snakeHead += UP;
+			break;
+
+		case DOWN:
+			grid[*snakeHead].isSnakeHead = false;
+			if(grid[*snakeHead + DOWN].isApple){
+				*length += 1;
+				*eatApple = true;
+				grid[*snakeHead + DOWN].removeApple();
+			}
+			if(grid[*snakeHead + DOWN].isSnake){
+				return true;
+			}
+			grid[*snakeHead + DOWN].setSnake();
+			grid[*snakeHead + DOWN].setSnakeHead(*length);
+			*snakeHead += DOWN;
+			break;
+
+		case LEFT:
+			grid[*snakeHead].isSnakeHead = false;
+			if(grid[*snakeHead + LEFT].isApple){
+				*length += 1;
+				*eatApple = true;
+				grid[*snakeHead - 1].removeApple();
+			}
+			if(grid[*snakeHead + LEFT].isSnake){
+				return true;
+			}
+			grid[*snakeHead + LEFT].setSnake();
+			grid[*snakeHead + LEFT].setSnakeHead(*length);
+			*snakeHead -= 1;
+			break;
+
+		case RIGHT:
+			grid[*snakeHead].isSnakeHead = false;
+			if(grid[*snakeHead + RIGHT].isApple){
+				*length += 1;
+				*eatApple = true;
+				grid[*snakeHead + RIGHT].removeApple();
+			}
+			if(grid[*snakeHead + RIGHT].isSnake){
+				return true;
+			}
+			grid[*snakeHead + RIGHT].setSnake();
+			grid[*snakeHead + RIGHT].setSnakeHead(*length);
+			*snakeHead += 1;
+			break;
+	}
+	return false;
+}
+
+void Movement(GLFWwindow* window){
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
+		currentDirection = UP;
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
+		currentDirection = DOWN;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
+		currentDirection = LEFT;
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
+		currentDirection = RIGHT;
+	}
+}
+
+void Draw(gridSpace* grid, bool* eatApple){
+	for(int i = 0; i < 400; i++){
+		if(grid[i].isSnake){
+			glBufferData(GL_ARRAY_BUFFER, sizeof(grid[i].gameCoords), grid[i].gameCoords, GL_STATIC_DRAW);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+			glDrawArrays(GL_TRIANGLES, 3, 3);
+			if(*eatApple){
+				spawnApple(grid);
+				*eatApple = false;
+				//printf("apple\n");
+				grid[i].removeApple();
+			}else{
+				grid[i].decrementSnake();
+			}
+		}
+		if(grid[i].isApple){
+			glBufferData(GL_ARRAY_BUFFER, sizeof(grid[i].gameCoords), grid[i].gameCoords, GL_STATIC_DRAW);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+			glDrawArrays(GL_TRIANGLES, 3, 3);
+		}
+	}
+	
+}
 
 int main(){
 	srand(time(0));
@@ -108,32 +202,26 @@ int main(){
 	glewExperimental = true;
 	glewInit();
 
-	// GLfloat box[] = {
-	// 	// first triangle
-	// 	0.05f,  0.05f, 0.0f,  // top right
-	// 	0.05f, -0.05f, 0.0f,  // bottom right
-	// 	-0.05f,  0.05f, 0.0f,  // top left 
-	// 	// second triangle
-	// 	0.05f, -0.05f, 0.0f,  // bottom right
-	// 	-0.05f, -0.05f, 0.0f,  // bottom left
-	// 	-0.05f,  0.05f, 0.0f   // top left
-	// };
-
-
-	Square* snake = new Square[400];
+	int length = 4;
 	gridSpace* grid = new gridSpace[400];
 	for(int i = 0; i < 400; i++){
 		grid[i].setGameCoords(i);
-		if(i < 400){
-			grid[i].setSnake();
-		}
 	}
+	grid[190].setSnake();
+	grid[189].setSnake();
+	grid[188].setSnake();
+	grid[187].setSnake();
+	grid[190].setSnakeHead(length);
+	grid[189].setSnakeHead(length);
+	grid[188].setSnakeHead(length);
+	grid[187].setSnakeHead(length);
+	grid[48].setApple();
 
-	grid[0].setSnake();
+	int snakeHead = 190;
+	bool eatApple = false;
 
-	int num = 0;
-
-	Shader shaderProgram("vertexShaders/default.vert", "fragmentShaders/default.frag");
+	Shader snakeShader("vertexShaders/default.vert", "fragmentShaders/snake.frag");
+	snakeShader.Activate();
 
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
@@ -143,64 +231,31 @@ int main(){
 	glGenBuffers(3, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-	// unsigned int EBO;
-	// glGenBuffers(1, &EBO);
-	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
 	glEnableVertexAttribArray(0);  
-
 
     float one = 0.07f, two = 0.13f, three = 0.17f;
 
     while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(one, two, three, 1.0f);
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		
+		Movement(window);
 
-		if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS){
-			snake[num].setBoxRand();
-			num++;
-		}
-		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+		if(setMovement(grid, &snakeHead, &eatApple, &length)){
 			break;
 		}
-		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
-			snake[0].moveUp();
-		}
-		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
-			snake[0].moveDown();
-		}
-		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
-			snake[0].moveLeft();
-		}
-		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
-			snake[0].moveRight();
-		}
-		
-		
-		shaderProgram.Activate();
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-		// for(int i = 0; i < num; i++){
-		// 	glBufferData(GL_ARRAY_BUFFER, sizeof(snake[i].box), snake[i].box, GL_STATIC_DRAW);
-		// 	glDrawArrays(GL_TRIANGLES, 0, 3);
-		// 	glDrawArrays(GL_TRIANGLES, 3, 3);
-		// }
+		Draw(grid, &eatApple);
 
-		for(int i = 0; i < 400; i++){
-			if(grid[i].isSnake){
-				//printf("%f\n", i);
-				glBufferData(GL_ARRAY_BUFFER, sizeof(grid[i].gameCoords), grid[i].gameCoords, GL_STATIC_DRAW);
-				glDrawArrays(GL_TRIANGLES, 0, 3);
-				glDrawArrays(GL_TRIANGLES, 3, 3);
-			}
+		if(snakeHead < 0 || snakeHead > 399){
+			break;
 		}
 
         glfwSwapBuffers(window);
 		glfwPollEvents();
-		Sleep(10);
+		Sleep(100);
 
     }
 }
